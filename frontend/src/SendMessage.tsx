63c08b4d59fb00feac015b4f61a095da2eef0176
lyumii <dmly1124@gmail.com>
2025-03-19 10:54:48 +0100

    reverting some more

--- frontend/src/SendMessage.tsx
+++ frontend/src/SendMessage.tsx
@@ -66,7 +66,7 @@ export default function SendMessage() {
 
       try {
         const msg = JSON.parse(textData);
-        if (!msg || !msg.type || !msg.date || !msg.text) return;
+        if (!msg || !msg.type) return;
         setMessages((prev) => [...prev, msg]);
       } catch (error) {
         console.warn("non json msg", event.data);
